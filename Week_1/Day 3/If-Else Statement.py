5383b9ebf5e87d03a42b20bbcba3c5cdf4c777b2
Devine <simsdevine@gmail.com>
2025-11-16 17:14:15 -0600

    25.11.16

--- Week_1/Day 3/If-Else Statement.py
+++ Week_1/Day 3/If-Else Statement.py
@@ -15,16 +15,22 @@ else:
 
 Python Indentation
 Understand the importance of indentation in Python as a way to make certain lines of code subsidaries of other lines of code.
-
-e.g.
 if 5 > 2: #This is a parent line of code
     print("yes") #this is a child line of code
 
 Comparator Operators
-> Greater than
-< Less than
->= Greater than or equal to
-<= Less than or equal to
-== Equal to
-!= Not equal to
+    > Greater than
+    < Less than
+    >= Greater than or equal to
+    <= Less than or equal to
+    == Equal to
+    != Not equal to
 """
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster.")
+else:
+    print("Sorry you have to grow taller before you can ride.")
